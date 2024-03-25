edffd7c4482866a6775df61bc3cb65f2619fb121
Soniq7 <ssoniq7@gmail.com>
2024-03-26 00:06:48 +0100

    HeaderTitle + HeaderText added and styled

--- src/common/SectionVideo/index.js
+++ src/common/SectionVideo/index.js
@@ -1,10 +1,20 @@
-import { StyledSection, Video } from "./styled";
+import { StyledSection, Video, HeaderText, HeaderTitle } from "./styled";
 
 const Section = ({ title, body, headerContent }) => (
   <StyledSection>
     <Video autoPlay playsInline loop muted>
-      <source src={process.env.PUBLIC_URL + "/backgroundVideo.mp4"} type="video/mp4"  />
+      <source
+        src={process.env.PUBLIC_URL + "/backgroundVideo.mp4"}
+        type="video/mp4"
+      />
     </Video>
+    <HeaderTitle>
+      <p>RWOLUCYJNY DRUK ŚCIENNY</p>
+    </HeaderTitle>
+    <HeaderText>
+      <p>VerticalPrint...odważ się!</p>
+      <p>Wyróżniaj się czymś unikalnym</p>
+    </HeaderText>
   </StyledSection>
 );
 
--- src/common/SectionVideo/styled.js
+++ src/common/SectionVideo/styled.js
@@ -17,3 +17,39 @@ export const Video = styled.video`
     margin-top: 80px;
   }
 `;
+export const HeaderTitle = styled.div`
+  position: absolute;
+  z-index: -1;
+  left: 5%;
+  transform: translate(0%, 60%);
+  color: ${({ theme }) => theme.colors.bondiBlue};
+  font-family: "Ubuntu Condensed", sans-serif;
+  font-weight: 700;
+  line-height: 8vw;
+  font-size: 7vw; /* Dostosuj rozmiar tekstu według potrzeb */
+  text-align: left; /* Wyśrodkuj tekst */
+  padding: 2vw; /* Dodaj wypełnienie dla lepszego wyglądu */
+  width: 50%; /* Szerokość tekstu */
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    transform: translate(0%, 90%);
+  }
+`;
+
+
+export const HeaderText = styled.div`
+  position: absolute;
+  z-index: -1;
+  left: 5%;
+  transform: translate(0%, 300%);
+  line-height: 1vw;
+  color: ${({ theme }) => theme.colors.alto};; /* Ustaw kolor tekstu na biały lub inny kontrastujący kolor */
+  font-size: 3vw; /* Dostosuj rozmiar tekstu według potrzeb */
+  text-align: left; /* Wyśrodkuj tekst */
+  padding: 2vw; /* Dodaj wypełnienie dla lepszego wyglądu */
+  width: 80%; /* Szerokość tekstu */
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+    transform: translate(0%, 370%);
+  }
+`;
