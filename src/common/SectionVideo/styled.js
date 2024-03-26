5b8bd33640501abf00a5aa1c2cf840f6c6b3cabb
Soniq7 <ssoniq7@gmail.com>
2024-03-26 03:49:08 +0100

    Background video height and width adjustment /HeaderTitle, HeadeText position and font size adjustment

--- src/common/SectionVideo/styled.js
+++ src/common/SectionVideo/styled.js
@@ -8,48 +8,51 @@ export const StyledSection = styled.div`
 
 export const Video = styled.video`
   position: absolute;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   object-fit: cover;
   z-index: -1;
-  margin-top: 100px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
-    margin-top: 80px;
+    width: 100%;
   }
 `;
 export const HeaderTitle = styled.div`
   position: absolute;
   z-index: -1;
-  left: 5%;
-  transform: translate(0%, 60%);
+  left: 2%;
+  bottom: -90vh;
   color: ${({ theme }) => theme.colors.bondiBlue};
   font-family: "Ubuntu Condensed", sans-serif;
   font-weight: 700;
-  line-height: 8vw;
-  font-size: 7vw; /* Dostosuj rozmiar tekstu według potrzeb */
+  font-size: 5vw; /* Dostosuj rozmiar tekstu według potrzeb */
   text-align: left; /* Wyśrodkuj tekst */
   padding: 2vw; /* Dodaj wypełnienie dla lepszego wyglądu */
-  width: 50%; /* Szerokość tekstu */
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
-    transform: translate(0%, 90%);
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
+    font-size: 6vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 8vw;
   }
 `;
-
 
 export const HeaderText = styled.div`
   position: absolute;
   z-index: -1;
-  left: 5%;
-  transform: translate(0%, 300%);
-  line-height: 1vw;
+  left: 2%;
+  bottom: -95vh;
   color: ${({ theme }) => theme.colors.alto};; /* Ustaw kolor tekstu na biały lub inny kontrastujący kolor */
-  font-size: 3vw; /* Dostosuj rozmiar tekstu według potrzeb */
+  font-size: 2vw; /* Dostosuj rozmiar tekstu według potrzeb */
   text-align: left; /* Wyśrodkuj tekst */
   padding: 2vw; /* Dodaj wypełnienie dla lepszego wyglądu */
-  width: 80%; /* Szerokość tekstu */
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
-    transform: translate(0%, 370%);
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
+    font-size: 3vw;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 4vw;
   }
 `;
