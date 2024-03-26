20df3624d0c024d0be6183959032c0fede7a266c
Soniq7 <ssoniq7@gmail.com>
2024-03-27 00:12:53 +0100

    component SectionAboutUs created

---
+++ src/common/SectionAboutUs/index.js
@@ -0,0 +1,19 @@
+
+
+const SectionAboutUs = () => (
+  <StyledSection>
+    <BackgroundPicture>
+      <source 
+      
+      />
+    </BackgroundPicture>
+    <Title>
+      <p></p>
+    </Title>
+    <Text>
+      <p></p>
+    </Text>
+  </StyledSection>
+);
+
+export default SectionAboutUs;
