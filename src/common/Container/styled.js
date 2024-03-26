3eb506490ddc9a267b586c89b4124dfe84907ae3
Soniq7 <ssoniq7@gmail.com>
2024-03-27 00:08:58 +0100

    Container component css amendment

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 const Main = styled.main`
-  max-width: 100%;
+  width: 100%;
   margin: 0 auto;
-  padding: 20px;
 `;
 
 const Container = ({ children }) => (
